05623b08c754f8af0023a670e8baf29ece7e5a13
Grione <greeb.spb@gmail.com>
2019-06-03 21:44:05 +0300

    codestyle

--- src/index.js
+++ src/index.js
@@ -2,41 +2,35 @@
 
 /*
  Задание 1:
-
  Напишите аналог встроенного метода forEach для работы с массивами
  Посмотрите как работает forEach и повторите это поведение для массива, который будет передан в параметре array
  */
 
-
 function forEach(array, fn) {
-  
-    for(let i = 0; i < array.length; i++) {
+
+    for (let i = 0; i < array.length; i++) {
         fn(array[i], i, array)
     }
 }
 
 /*
  Задание 2:
-
  Напишите аналог встроенного метода map для работы с массивами
  Посмотрите как работает map и повторите это поведение для массива, который будет передан в параметре array
  */
 
-
 function map(array, fn) {
     let newArr = [];
-    for(let i = 0; i < array.length; i++) {
+
+    for (let i = 0; i < array.length; i++) {
         newArr[i] = fn(array[i], i, array);
-        
     }
+
     return newArr;
 }
 
-
-
 /*
  Задание 3:
-
  Напишите аналог встроенного метода reduce для работы с массивами
  Посмотрите как работает reduce и повторите это поведение для массива, который будет передан в параметре array
  */
@@ -53,44 +47,37 @@ function reduce(array, fn, initial) {
 
 /*
  Задание 4:
-
  Функция должна перебрать все свойства объекта, преобразовать их имена в верхний регистр и вернуть в виде массива
-
  Пример:
    upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
  */
 function upperProps(obj) {
     let array = [];
-    for (var key in obj) {
-        let str = "";
-        str = key.toUpperCase();
 
+    for (var key in obj) {
+        let str = ''; 
+
+        str = key.toUpperCase();
         array.push(str);
     }
 
     return array;
-    
+
 }
-
-
 
 /*
  Задание 5 *:
-
  Напишите аналог встроенного метода slice для работы с массивами
  Посмотрите как работает slice и повторите это поведение для массива, который будет передан в параметре array
  */
-function slice(array, from, to) {
-}
+function slice(array, from, to) {}
 
 /*
  Задание 6 *:
-
  Функция принимает объект и должна вернуть Proxy для этого объекта
  Proxy должен перехватывать все попытки записи значений свойств и возводить это значение в квадрат
  */
-function createProxy(obj) {
-}
+function createProxy(obj) {}
 
 export {
     forEach,
